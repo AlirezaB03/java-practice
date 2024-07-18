391b6034dccd40d78a7f67bdb0fe9ff755ebead2
amir <amirbahraami99@gmail.com>
2024-07-18 12:56:23 +0330

    make two class HistoryCalc and IHistory

---
+++ src/main/java/calc/HistoryCalc.java
@@ -0,0 +1,11 @@
+package calc;
+
+import java.io.File;
+
+public class HistoryCalc {
+    public static void main(String[] args) {
+        File path = new File("/home/amir/Documents/alireza/project/java-practice/Test2.java");
+
+
+    }
+}
