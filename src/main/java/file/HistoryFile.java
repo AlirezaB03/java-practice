3e111e9c6c5c037a46be96c29e9e961e1ef08ca0
amir <amirbahraami99@gmail.com>
2024-07-19 14:39:58 +0330

    add new directory ( src/main/java/file/HistoryFile.java)

--- src/main/java/file/HistoryFile.java
+++ src/main/java/file/HistoryFile.java
@@ -1,4 +1,18 @@
 package file;
 
+import java.io.File;
+import java.io.IOException;
+
 public class HistoryFile {
+    public static void main(String[] args) throws IOException {
+        File file = new File("D://HistoryCalc");
+        File textfile = new File(file, "HistoryCalc.txt");
+        if (!file.exists()) {
+            file.mkdirs();
+        }
+
+        if (!textfile.exists()) {
+            textfile.createNewFile();
+        }
+    }
 }
