bd444b37d59c0a79a1b61da955f001e592fe9a21
amir <amirbahraami99@gmail.com>
2024-07-21 21:35:09 +0330

    add print

--- src/main/java/lern/file/FileWrite/TestWrite.java
+++ src/main/java/lern/file/FileWrite/TestWrite.java
@@ -6,5 +6,6 @@ public class TestWrite {
     public static void main(String[] args) throws IOException {
         FileWrite file = new FileWrite();
         file.r();
+        System.out.println("dddddd");
     }
 }
